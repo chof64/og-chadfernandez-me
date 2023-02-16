59d0dc1af635a29ca273f8b29265e1dc64fff9d4
Chad Fernandez <contact@chadfernandez.me>
2023-02-17 01:34:49 +0800

    update chadfernandez-me image design

--- src/pages/api/chadfernandez-me/index.js
+++ src/pages/api/chadfernandez-me/index.js
@@ -1,5 +1,3 @@
-// pages/api/og/custom-font.jsx
-
 import { ImageResponse } from "@vercel/og";
 
 export const config = {
@@ -23,17 +21,35 @@ export default async function handler(req) {
 
     return new ImageResponse(
       (
-        <div tw="flex w-full h-full flex-col items-center bg-white justify-center">
-          <h1 tw="text-6xl font-bold">Chad Fernandez</h1>
-          <p tw="text-center max-w-xl text-neutral-700 font-medium">
-            A web developer and a student from the Philippines. And this is my
-            own little space in this vast world wide web. I write about
-            programming and the world of technology. I also share things that
-            I&apos;ve learned along the way.
-          </p>
-          <p tw="absolute bottom-0 text-blue-500 font-semibold">
-            https://chadfernandez.me
-          </p>
+        <div tw="flex items-end justify-center w-full h-full bg-sky-300">
+          {/* WINDOW BOX */}
+          <div tw="bg-white flex-col flex w-[80%] h-[90%] rounded-t-lg shadow-2xl">
+            {/* WINDOW NAV BAR */}
+            <div tw="flex relative items-center w-full bg-gray-100 py-2 rounded-t-lg justify-center border-b border-neutral-200">
+              {/* BUTTONS */}
+              <div tw="flex absolute left-4">
+                <div tw="p-1.5 mx-0.5 bg-red-500 rounded-full border-[0.5px] border-red-700" />
+                <div tw="p-1.5 mx-0.5 bg-amber-400 rounded-full border-[0.5px] border-amber-600" />
+                <div tw="p-1.5 mx-0.5 bg-green-600 rounded-full border-[0.5px] border-green-800" />
+              </div>
+              {/* URL BAR */}
+              <div tw="flex w-[400px] py-1 text-sm justify-center text-neutral-700 bg-gray-200 rounded-md border-[0.5px] border-neutral-300">
+                chadfernandez.me
+              </div>
+            </div>
+            {/* WINDOW CONTENT */}
+            <div tw="flex flex-col h-full w-full">
+              <div tw="flex flex-col items-center justify-center w-full mt-40">
+                <div tw="flex max-w-lg text-6xl font-bold">Chad Fernandez</div>
+                <div tw="flex max-w-xl mt-4 text-neutral-700 items-center text-center">
+                  I am a web developer and a student from the Philippines. And
+                  this is my own little space in this vast world wide web. I
+                  write about programming and the world of technology. I also
+                  share things that I&apos;ve learned along the way.
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       ),
       {
